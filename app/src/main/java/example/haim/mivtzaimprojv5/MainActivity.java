bb3fdea180a7bb0b5b2d98cf873d576043d5fbef
chaimon771 <chaimon771@gmail.com>
2018-02-14 09:24:50 +0200

    Registration + Profile - Done (-> up to Welcome - required design graphic)

--- app/src/main/java/example/haim/mivtzaimprojv5/MainActivity.java
+++ app/src/main/java/example/haim/mivtzaimprojv5/MainActivity.java
@@ -26,6 +26,7 @@ import example.haim.mivtzaimprojv5.fragments.StartTableFragment;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+//haim's test for github
 
     private FirebaseUser user;
     @BindView(R.id.progressBar)
